51442647854328effc432cde1dc8eac85251fd3f
Alejandro Biarrieta <103573136+Reinapepeada@users.noreply.github.com>
2024-06-18 13:51:30 -0300

    trayendo un provedor por cuit working

--- src/app/proveedores/[slug]/page.tsx
+++ src/app/proveedores/[slug]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useEffect, useState } from "react";
-import  getProveedores  from "../../../actions/getProveedores";
+import  getProveedores  from "../../../actions/getProveedorXCuit";
+import getProveedorXCuit from "../../../actions/getProveedorXCuit";
 
 export default function Page({ params }: { params: { slug: string } }) {
     const [providers, setProviders] = useState([]);
@@ -8,8 +9,9 @@ export default function Page({ params }: { params: { slug: string } }) {
     useEffect(() => {
         const fetchProviders = async () => {
             try {
-                const response = await getProveedores();
+                const response = await getProveedorXCuit(params.slug);
                 setProviders(response);
+                console.log("Proveedores:", response);
             } catch (error) {
                 console.error("Error al obtener los proveedores:", error);
             }
@@ -20,10 +22,10 @@ export default function Page({ params }: { params: { slug: string } }) {
     return (
         <div style={{ padding: "10rem" }}>
             My Post: {params.slug}
-            <h1>Proveedores</h1>
+            <h1>Proveedor</h1>
             <ul>
                 {providers.map((provider: any) => (
-                    <li key={provider.cuit}>{provider.nombreProveedor}</li>
+                    <><li key={provider.cuit}>{provider.nombreProveedor}</li><li key={provider.cuit}>{provider.cuit}</li><li key={provider.cuit}>{provider.reputacion}</li></>
                 ))}
             </ul>
         </div>
