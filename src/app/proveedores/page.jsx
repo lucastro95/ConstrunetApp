e2af33242e7fa8d147b39beb5c03298dca33af44
Lucía Castro <90776369+lucastro95@users.noreply.github.com>
2024-06-18 13:23:40 -0300

    ruta proveedores

---
+++ src/app/proveedores/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './proveedores.module.scss'
+
+const page = () => {
+  return (
+    <main className={styles.main}>
+        <div className={styles.layout}>
+            <h2>Mis peticiones</h2>
+            
+        </div>
+    </main>
+  )
+}
+
+export default page
